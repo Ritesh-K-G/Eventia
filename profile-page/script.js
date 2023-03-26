868e8d9344db02018ae0e7e80c600050d18b0fb6
parthgarg497 <parthgarg497@gmail.com>
2023-03-26 23:58:51 +0530

    Profile image is now updating

--- profile-page/script.js
+++ profile-page/script.js
@@ -1,12 +1,14 @@
-document.getElementById("proimg").addEventListener("contextmenu",function(event){
+let image = document.getElementById("proimg");
+image.addEventListener("contextmenu",function(event){
     // console.log(event.offsetX,event.offsetY);
     event.preventDefault();
-    var contextmenu = document.getElementById("context-menu");
-    contextmenu.style.top = event.offsetY+"px";
-    contextmenu.style.left = event.offsetX+"px";
+    let contextmenu = document.getElementById("context-menu");
+    contextmenu.style.top += event.pageY;
+    contextmenu.style.left += event.pageX;
     contextmenu.classList.add("active");
 });
 window.addEventListener("click",function(){
-    var contextmenu = document.getElementById("context-menu");
+    let contextmenu = document.getElementById("context-menu");
     contextmenu.classList.remove("active");
 });
+
