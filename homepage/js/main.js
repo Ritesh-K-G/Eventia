3b8f74938ffe9557ed18c8ed9d191b2cb1714810
Adityatmr123 <mrx9918261025@gmail.com>
2023-04-17 20:51:03 +0530

    added notification bell and fixed card size

--- homepage/js/main.js
+++ homepage/js/main.js
@@ -195,3 +195,19 @@ $(document).ready(function() {
       $(this).find('#notify').toggle();
     });
 });
+// notification
+var box  = document.getElementById('box');
+var down = false;
+
+
+function toggleNotifi(){
+	if (down) {
+		box.style.height  = '0px';
+		box.style.opacity = 0;
+		down = false;
+	}else {
+		box.style.height  = '510px';
+		box.style.opacity = 1;
+		down = true;
+	}
+}
